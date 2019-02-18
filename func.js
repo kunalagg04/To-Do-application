47a8c9c59ac700c5044e886856dde1e39865015c
KunaL AggarwaL <kunalagg04@gmail.com>
2019-02-18 18:51:10 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,26 @@
+$("ul").on("click", "li",function(){
+    
+    $(this).toggleClass("completed");
+})
+
+$("ul").on("click", "span" ,function(event){
+    debugger;
+    $(this).parent().fadeOut(500, function(){
+       $(this).remove(); 
+    });                     
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
+	}
+});
+$(".far").click(function(){
+    $("input").fadeToggle();
+})
+
